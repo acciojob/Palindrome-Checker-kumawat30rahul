2017bb2f2de21bcbd3a3d78bc06eb7e24b80b676
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-01-11 21:31:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,12 +2,17 @@
 
 function palindrome(str){
 	let str1 = str.toLowerCase()
+	let flag = 1;
 	for(let i = 0;i < str.length;i++){
 		if(str1[i] != str1[str1.length - i]){
-			return false;
+			flag = 0;
 		}
 	}
 
-	return true
+	if(flag == 1){
+		return true
+	}else{
+		return false
+	}
 }
 module.exports = palindrome
