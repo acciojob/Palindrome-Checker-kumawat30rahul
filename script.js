3a1b54a902a1b7fd006ccfd1862630cd8d7d9ee7
Rahul Kumawat <116284606+kumawat30rahul@users.noreply.github.com>
2023-01-11 21:26:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 // complete the given function
 
 function palindrome(str){
+	let str1 = str.toLowerCase()
 	for(let i = 0;i < str.length;i++){
-		if(str[i] != str[str.length - i]){
+		if(str1[i] != str1[str1.length - i]){
 			return false;
 		}
 	}
